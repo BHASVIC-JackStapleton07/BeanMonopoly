73e37d8522d6969960edeaa0fe2c9c34d5ddc98b
BHASVIC-Thomassmyth23 <thomas.smyth23@students.bhasvic.ac.uk>
2024-03-06 08:55:35 +0000

    quick player change

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,7 @@
 package src;
 
 public class Player {
-    int MoneyBalance = 0; //decide on starting balance later
+    int MoneyBalance; //decide on starting balance later
     int PlayerNumber; //stores player number
     String PlayerName;  //stores name of the player
 
@@ -13,14 +13,9 @@ public class Player {
         this.PlayerNumber = playerNumber;
         this.PlayerName = playerName;
     }
-    public int addMoney(int moneyAmmount) {  //takes certain amount of money and passes it into method so that method knows how much money to add (eg 1000 for landing on go 500 for passing go)
-        MoneyBalance = MoneyBalance + moneyAmmount;
+    public int changeMoney(int moneyAmmount) {  //takes certain amount of money and passes it into method so that method knows how much money to add (eg 1000 for landing on go 500 for passing go)
+        MoneyBalance = MoneyBalance + moneyAmmount; //can pass in negative parameter to decrease money
         return MoneyBalance;
     }
-    public int decreaseMoney(int moneyAmmount) {
-        MoneyBalance = MoneyBalance - moneyAmmount;
-        return MoneyBalance;
-    }
-
 }
 
