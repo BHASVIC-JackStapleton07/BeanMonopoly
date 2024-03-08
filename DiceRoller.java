de32a1178058a76fb38e6f572730c9e90f0c3c2d
BHASVIC-HugoWilkinson23 <hugo.wilkinson23@students.bhasvic.ac.uk>
2024-03-08 11:02:33 +0000

    ?

---
+++ DiceRoller.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+        public class DiceRoller
+        {
+        private int dice1;
+        private int dice2;
+        private boolean isDouble;
+
+        public DiceRoller()
+        {
+            rollDice();
+        }
+
+        public void rollDice()
+        {
+            Random random = new Random();
+            dice1 = random.nextInt(6) + 1;
+            dice2 = random.nextInt(6) + 1;
+            isDouble = dice1 == dice2;
+        }
+
+        public int getTotalScore()
+        {
+            return dice1 + dice2;
+        }
+
+        public boolean isDouble()
+        {
+            return isDouble;
+        }
+
+        public static void main(String[] args)
+        {
+                DiceRoller diceRoller = new DiceRoller();
+                System.out.println("Dice 1: " + diceRoller.dice1);
+                System.out.println("Dice 2: " + diceRoller.dice2);
+                System.out.println("Total Score: " + diceRoller.getTotalScore());
+                System.out.println("Is double: " + diceRoller.isDouble());
+        }
+        }
